afbfa8fde605d44d734e09beb92998695ebbfdd8
Elvin Camalzade <e.camalzade@itstep.edu.az>
2021-10-20 17:33:33 +0400

    Not done

---
+++ ProjectWithMvvm/Domain/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWithMvvm.Domain.ViewModels
+{
+   public class MainViewModel:BaseViewModel
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}
