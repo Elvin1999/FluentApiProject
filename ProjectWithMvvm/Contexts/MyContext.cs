34b0a5832a8e4f1da0afd089b1d447899eb69e89
Elvin Camalzade <e.camalzade@itstep.edu.az>
2021-10-20 16:22:44 +0400

    Add project files.

---
+++ ProjectWithMvvm/Contexts/MyContext.cs
@@ -0,0 +1,42 @@
+﻿using ProjectWithMvvm.Entities;
+using ProjectWithMvvm.Entities.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWithMvvm.Contexts
+{
+    public class MyContext:DbContext
+    {
+        public MyContext()
+        {
+            this.Database.Connection.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        { 
+            //Customers.Add(new Customer
+            //{
+            //    City = "Baku",
+            //    Country = "Azerbaijan",
+            //    ContactName = "Az-Telecom",
+            //    CompanyName = "StepIT Academy"
+            //});
+            //Orders.Add(new Order
+            //{
+            //     CustomerId=1,
+            //      OrderDate=DateTime.Now
+            //});
+            modelBuilder.Configurations.Add(new CustomerMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+           
+        }
+    }
+}
